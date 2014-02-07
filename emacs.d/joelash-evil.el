253a26ee8283eddddd345ba0602f4f47bfe48490
Joel Friedman <asher.friedman@gmail.com>
2014-02-07 10:04:32 -0600

    adding evil-nerd-commenter and evil-numbers

--- emacs.d/joelash-evil.el
+++ emacs.d/joelash-evil.el
@@ -50,4 +50,15 @@
 
 ;(add-hook 'prog-mode-hook 'evil-close-folds)
 
+;; Plugins
+
+;; Numbers
+(package-require 'evil-numbers)
+(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+
+;; Nerd commenter
+(package-require 'evil-nerd-commenter)
+(evilnc-default-hotkeys)
+
 (provide 'joelash-evil)
