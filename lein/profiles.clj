77ed6df0e4a20e53a4c2c3009f7d978bbc7b8c73
Joel Friedman <asher.friedman@gmail.com>
2014-11-25 09:04:33 -0500

    android clojure in lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,11 @@
 {:user {:plugins [[org.clojars.weavejester/lein-clojars "0.8.0"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-autoexpect "1.0"]
                   [lein-marginalia "0.7.1"]
                   [lein-exec "0.3.1"]
-                  [lein-droid "0.2.3"]]
-        :android {:sdk-path "/usr/local/Cellar/android-sdk/23.0.2"}
+                  [lein-droid "0.2.4-SNAPSHOT"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :jvm-opts ["-XX:+CMSClassUnloadingEnabled" "-XX:MaxPermSize=256M"]}}
+        :jvm-opts ["-XX:+CMSClassUnloadingEnabled" "-XX:MaxPermSize=256M"]}
+
+ :android-config {:android {:sdk-path "/usr/local/Cellar/android-sdk/23.0.2"}}}
