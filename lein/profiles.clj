46f476f1549f37214aefacf08d0dbc0cac942502
Joel Friedman <asher.friedman@gmail.com>
2014-12-16 11:41:42 -0600

    remove lein-droid from profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,8 +2,7 @@
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-autoexpect "1.0"]
                   [lein-marginalia "0.7.1"]
-                  [lein-exec "0.3.1"]
-                  [lein-droid "0.2.4-SNAPSHOT"]]
+                  [lein-exec "0.3.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :jvm-opts ["-XX:+CMSClassUnloadingEnabled" "-XX:MaxPermSize=256M"]}
