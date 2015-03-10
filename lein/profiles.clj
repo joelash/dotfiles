3dc430dcc7fa030aa63f032adc91f825ae898daf
Joel Friedman <asher.friedman@gmail.com>
2015-03-10 14:05:56 -0500

    random lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,20 @@
-{:user {:plugins [[org.clojars.weavejester/lein-clojars "0.8.0"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-autoexpect "1.0"]
+                  ;[jonase/eastwood "0.2.1"]
+                  [lein-exec "0.3.1"]
                   [lein-marginalia "0.7.1"]
-                  [lein-exec "0.3.1"]]
-        :dependencies [[slamhound "1.5.5"]]
+                ;_[lein-cljfmt "0.1.6"]
+                  [org.clojars.weavejester/lein-clojars "0.8.0"]
+                ;_[venantius/ultra "0.1.9"]
+                ]
+        :ultra  {:color-scheme :solarized_dark}
+        :cljfmt {:indents
+                 {require [[:block 0]]
+                  ns [[:block 0]]}}
+        :dependencies [[slamhound "1.5.5"]
+                      ;_[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                      ;_[cljfmt "0.1.6"]
+                      ]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :jvm-opts ["-XX:+CMSClassUnloadingEnabled" "-XX:MaxPermSize=256M"]}
 
