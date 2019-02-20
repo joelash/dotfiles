55381f95fe2a49a72d34fada7c477ade9a1e58ad
Joel Friedman <asher.friedman@gmail.com>
2019-02-20 08:24:26 -0600

    Adds ssh aliases for sdm

--- bash/20-aclaimant.sh
+++ bash/20-aclaimant.sh
@@ -3,6 +3,8 @@ function acl-env {
   # unset DOCKER_CERT_PATH
   # export DOCKER_HOST=tcp://192.168.33.42:2375
   export AWS_PROFILE=acl
+  alias ssh="/usr/local/bin/sdm ssh wrapped-run"
+  alias scp="scp -S'/usr/local/bin/sdm' -osdmSCP"
   . ~/src/aclaimant/Envfile
 }
 
